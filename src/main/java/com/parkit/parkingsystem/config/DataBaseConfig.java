f2e04b8575d15768e240897a2f0d650229f6e3cc
Israel <meir950216@gmail.com>
2020-12-10 15:39:57 +0100

    configuration de l'heurs de la DB

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -13,7 +13,7 @@ public class DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod?serverTimezone=UTC","root","MYsql123!");
+                "jdbc:mysql://localhost:3306/prod?serverTimezone=Europe/Paris","root","MYsql123!");
     }
 
     public void closeConnection(Connection con){
