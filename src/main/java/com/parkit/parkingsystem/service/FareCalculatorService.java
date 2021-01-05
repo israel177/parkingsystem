8fbb5929920c3002ef8fc6644bbf9e0d00108e93
Israel <meir950216@gmail.com>
2021-01-05 12:07:01 +0100

    recurring

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,12 +1,22 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
 
 import java.util.Date;
 
 public class FareCalculatorService {
     double one_minute_in_millisecondes = 60 * 1000;
+    private TicketDAO ticketDAO;
+
+    public TicketDAO getTicketDAO() {
+        return ticketDAO;
+    }
+
+    public void setTicketDAO(TicketDAO ticketDAO) {
+        this.ticketDAO = ticketDAO;
+    }
 
     public void calculateFare(Ticket ticket) {
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
@@ -24,8 +34,8 @@ public class FareCalculatorService {
         if (the_final_time_in_minutes <= 30) {
             time_less_than_thirty_minutes = 0;
             fare(ticket, time_less_than_thirty_minutes);
-        } else if (ticket.recurring) {
-            time_less_than_thirty_minutes = time_less_than_thirty_minutes - time_less_than_thirty_minutes * 5 / 100;
+        } else if (ticketDAO.seeIfRecurrent(ticket.getVehicleRegNumber())) {
+            time_less_than_thirty_minutes = time_less_than_thirty_minutes * 0.95;
             fare(ticket, time_less_than_thirty_minutes);
         } else {
             fare(ticket, time_less_than_thirty_minutes);
